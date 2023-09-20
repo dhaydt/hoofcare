32da9f6685a7a21a1254b2832790f9d1af43e038
dhaydt <1992dayat@gmail.com>
2023-09-20 10:59:41 +0700

    fix login btn

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -5,7 +5,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav me-auto w-100 mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link {{ $active == 'home' ? 'active' : '' }}" aria-current="page" href="{{ route('home') }}">Home</a>
         </li>
@@ -14,26 +14,22 @@
           <a class="nav-link" href="javascript:">{{ $c['name'] }}</a>
         </li>
         @endforeach
+        @if (auth()->user())
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            @if (auth()->user())
               <span>{{ auth()->user()->name }}</span>
-            @else
-              <span>Login</span>
-            @endif
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            @if (auth()->user())
             <li><a class="dropdown-item" href="#">Profile</a></li>
             <form action="{{ route('logout') }}" method="POST">
               @csrf
               <li><button class="dropdown-item" type="submit">Logout</button></li>
             </form>
-            @else
-            <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
-            @endif
           </ul>
         </li>
+        @else
+        <a href="{{ route('login') }}" class="btn btn-sm btn-outline-secondary ms-auto"><p class="my-1">Sign In</p></a>
+        @endif
       </ul>
       {{-- <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
