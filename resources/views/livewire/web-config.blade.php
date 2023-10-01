cd2a90453031482cf2bd58c842ab862b33dcd222
dhaydt <1992dayat@gmail.com>
2023-10-01 17:09:36 +0700

    fix web config

--- resources/views/livewire/web-config.blade.php
+++ resources/views/livewire/web-config.blade.php
@@ -180,7 +180,7 @@
                     </div>
                 </div>
             </div>
-            <div style="--col-span-default: span 1 / span 1;" class="col-[--col-span-default]">
+            {{-- <div style="--col-span-default: span 1 / span 1;" class="col-[--col-span-default]">
                 <div class="fi-fo-field-wrp">
                     <div class="grid gap-y-2">
                         <div class="flex items-center justify-between gap-x-3">
@@ -220,9 +220,9 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
-        <div class="fi-form-actions">
+        <div class="fi-form-actions" style="margin-top: 20px;">
             <div class="fi-ac gap-3 flex flex-wrap items-center justify-start">
                 <button style="--c-400:var(--primary-400);--c-500:var(--primary-500);--c-600:var(--primary-600);"
                     class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 dark:bg-custom-500 dark:hover:bg-custom-400 focus:ring-custom-500/50 dark:focus:ring-custom-400/50 fi-ac-btn-action"
