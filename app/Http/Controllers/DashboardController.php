bf22cdba92ed3b0b74446a2553fd34a0cf7f6f4e
dhaydt <1992dayat@gmail.com>
2023-10-04 16:22:54 +0700

    fix counter

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -80,7 +80,7 @@ class DashboardController extends Controller
 
             $flip1->name = $name;
             $flip1->file = Helpers::savePdf($dir, $name, $pdf);
-            $flip1->count = $files1 ?? 0;
+            $flip1->count = count($files1) ?? 0;
 
             $flip1->save();
 
@@ -116,10 +116,10 @@ class DashboardController extends Controller
 
             $flip2->name = $name;
             $flip2->file = Helpers::savePdf($dir, $name, $pdf);
-            $flip2->count = $files2 ?? 0;
+            $flip2->count = count($files2) ?? 0;
 
             $flip2->save();
-            
+
             Helpers::deletePdf($item['file2']['file'] ?? 'null');
 
 
