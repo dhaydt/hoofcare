9ebd94cd23f468cd426f0fe65e91b17feac2b0ec
dhaydt <1992dayat@gmail.com>
2023-11-01 15:29:01 +0700

    fix back button and home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,14 @@
     <title>{{ $title ?? 'Home' }}</title>
     @include('layouts.partials._head')
     @livewireStyles
+    <style>
+      .floating{
+        position: fixed;
+        bottom: 0;
+        background: #969696;
+        border-radius: 4px 4px 0 0;
+      }
+    </style>
 </head>
 
 <body id="kt_body" class="d-flex justify-content-center">
@@ -18,6 +26,10 @@
     <div class="container mt-4 px-4 pb-4">
       @yield('content')
     </div>
+  </div>
+  <div class="floating p-2">
+    <a href="{{ url()->previous() }}" data-bs-toggle="tooltip" title="Back" class="btn btn-sm btn-warning me-2"> <i class="fa-solid fa-arrow-left"></i></a>
+    <a href="{{ route('home') }}" data-bs-toggle="tooltip" title="Home" class="btn btn-sm btn-warning"> <i class="fa-solid fa-house"></i></a>
   </div>
   @include('layouts.partials._foot')
   @livewireScripts
